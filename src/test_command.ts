fc824b7fb6b5e80d892a716013c02517c9aab074
jeremy-rifkin <jeremy-rifkin@users.noreply.github.com>
2022-03-26 00:01:15 +0000

    [Workflow] Changes from upstream

--- src/test_command.ts
+++ src/test_command.ts
@@ -6,25 +6,25 @@ import { color, is_authorized_admin, TCCPP_ID } from "./common";
 let client: Discord.Client;
 
 function on_message(message: Discord.Message) {
-	if(message.author.id == client.user!.id) return; // Ignore self
-	if(message.author.bot) return; // Ignore bots
-	if(message.guildId != TCCPP_ID) return; // Ignore messages outside TCCPP (e.g. dm's)
-	if(message.content == "!wtest") {
-		assert(message.member != null);
-		if(is_authorized_admin(message.member)) {
-			message.reply(`test`);
-			const embed = new Discord.MessageEmbed()
-						 .setColor(color)
-						 .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL())
-						 .setDescription(`test test`)
-						 .setFooter(`ID: ${message.author.id}`)
-						 .setTimestamp();
-			message.channel.send({ embeds: [embed] });
-		}
-	}
+    if(message.author.id == client.user!.id) return; // Ignore self
+    if(message.author.bot) return; // Ignore bots
+    if(message.guildId != TCCPP_ID) return; // Ignore messages outside TCCPP (e.g. dm's)
+    if(message.content == "!wtest") {
+        assert(message.member != null);
+        if(is_authorized_admin(message.member)) {
+            message.reply(`test`);
+            const embed = new Discord.MessageEmbed()
+                         .setColor(color)
+                         .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL())
+                         .setDescription(`test test`)
+                         .setFooter(`ID: ${message.author.id}`)
+                         .setTimestamp();
+            message.channel.send({ embeds: [embed] });
+        }
+    }
 }
 
 export async function setup_test_command(_client: Discord.Client) {
-	client = _client;
-	client.on("messageCreate", on_message);
+    client = _client;
+    client.on("messageCreate", on_message);
 }
