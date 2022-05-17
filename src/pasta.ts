1f8ad9fa9891ac757038fc69d3e7551c3fedc1b6
jeremy-rifkin <jeremy-rifkin@users.noreply.github.com>
2022-05-17 06:59:27 +0000

    [Workflow] Changes from upstream

---
+++ src/pasta.ts
@@ -0,0 +1,16 @@
+import * as Discord from "discord.js";
+import { strict as assert } from "assert";
+import { M } from "./utils";
+import { color, is_authorized_admin, TCCPP_ID } from "./common";
+
+let client: Discord.Client;
+
+function on_interaction_create(interaction: Discord.Interaction) {
+    if (!interaction.isCommand()) return;
+    console.log(interaction);
+}
+
+export async function setup_pasta(_client: Discord.Client) {
+    client = _client;
+    client.on('interactionCreate', on_interaction_create);
+}
