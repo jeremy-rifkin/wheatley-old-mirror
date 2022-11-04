d0487e197fba5cb95535b493cbf59cab53d70185
jeremy-rifkin <jeremy-rifkin@users.noreply.github.com>
2022-11-04 21:37:49 +0000

    [Workflow] Changes from upstream

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
 # Wheatley
 
-[![build](https://github.com/jeremy-rifkin/wheatly/actions/workflows/build.yml/badge.svg)](https://github.com/jeremy-rifkin/wheatly/actions/workflows/build.yml)
-[![test](https://github.com/jeremy-rifkin/wheatly/actions/workflows/test.yml/badge.svg)](https://github.com/jeremy-rifkin/wheatly/actions/workflows/test.yml)
-[![eslint](https://github.com/jeremy-rifkin/wheatly/actions/workflows/eslint.yml/badge.svg)](https://github.com/jeremy-rifkin/wheatly/actions/workflows/eslint.yml)
+
+
+[![build](https://github.com/jeremy-rifkin/wheatly-mirror/actions/workflows/build.yml/badge.svg)](https://github.com/jeremy-rifkin/wheatly-mirror/actions/workflows/build.yml)
+
+[![test](https://github.com/jeremy-rifkin/wheatly-mirror/actions/workflows/test.yml/badge.svg)](https://github.com/jeremy-rifkin/wheatly-mirror/actions/workflows/test.yml)
+
+[![eslint](https://github.com/jeremy-rifkin/wheatly-mirror/actions/workflows/eslint.yml/badge.svg)](https://github.com/jeremy-rifkin/wheatly-mirror/actions/workflows/eslint.yml)
+
+
 
 This repository contains the source code for the Wheatley bot, made for the Together C & C++ discord
+
 server.
 
+
+
 This bot is primary internal moderation and administrative tools, such as tracking server
+
 suggestions and addressing bot raids and scammers. Scams and bot raids usually are not targeted, but
+
 the exact techniques and heuristics used to identify raids are, at least for now, omitted from the
+
 public mirror for this repository. The mirror pulls new changes daily.
+
+
 
 Copyright 2021-2022, all rights reserved.
--- mirror_tools/cleanse.py
+++ mirror_tools/cleanse.py
@@ -1,54 +1,113 @@
 import os
 
+
+
 sensitive_tag = "/** sensitive */"
 
+
+
 redacted_message = \
+
 f"""
+
 {sensitive_tag}
+
 /****************************************************
+
  * This file's contents have been stripped from the *
+
  * public mirror due to its sensitive nature. In    *
+
  * this repository files are considered sensitive   *
+
  * if they contain logic related to detection and   *
+
  * handling of scammers, spammers, and bots.        *
+
  ***************************************************/
+
 """.strip().split("\n") + [""]
 
+
+
 def emplace_over(a, b):
+
     return a + b[len(a):]
 
+
+
 def cleanse(path):
+
     if os.path.exists(path):
+
         with open(path, "r") as f:
+
             lines = ["/**/" for _ in f]
+
         lines = emplace_over(redacted_message, lines)
+
         with open(path, "w", encoding="utf-8") as f:
+
             f.write("\n".join(lines) + "\n")
+
         print(f"    cleansed {path}")
+
     else:
+
         print(f"    Error: Couldn't find {path}")
 
+
+
 def main():
-    for path, directories, files in os.walk("src"):
+
+    exclude = set([".git"])
+
+    for path, directories, files in os.walk("."):
+
+        directories[:] = [d for d in directories if d not in exclude] # https://stackoverflow.com/a/19859907/15675011
+
         for file_name in files:
+
             file_path = os.path.join(path, file_name)
+
             print(f"checking {file_path}")
+
             with open(file_path, "r", encoding="utf-8") as f:
+
                 lines = f.readlines()
+
                 if len(lines) > 0 and lines[0].strip() == sensitive_tag:
+
                     cleanse(file_path)
+
                 else:
+
                     changed_anything = False
+
                     for i, line in enumerate(lines):
+
                         new_line = line.replace(
-                            "https://github.com/jeremy-rifkin/wheatly/",
+
+                            "https://github.com/jeremy-rifkin/wheatly-mirror/",
+
                             "https://github.com/jeremy-rifkin/wheatly-mirror/"
+
                         )
+
                         if new_line != line:
+
                             lines[i] = new_line
+
                             changed_anything = True
+
                     if changed_anything:
+
+                        print(f"rewriting links in {file_path}")
+
                         with open(file_path, "w", encoding="utf-8") as f:
+
                             f.write("\n".join(lines))
+
+
 
 main()
